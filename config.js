0b2dbba705f6f01104e0346df01ffce480d85de3
Nate Garrett <nate.garrett1992@gmail.com>
2017-10-27 11:22:56 -0500

    added in authentication with JWT; still needs some work

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                      'mongodb://localhost/basic-auth-demo';
-exports.TEST_DATABASE_URL = (
-  process.env.TEST_DATABASE_URL ||
- 'mongodb://localhost/test-blog-app');
+exports.DATABASE_URL =
+    process.env.DATABASE_URL ||
+    global.DATABASE_URL ||
+    'mongodb://localhost/jwt-auth-demo';
 exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
